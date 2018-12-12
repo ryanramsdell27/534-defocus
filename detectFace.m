fc913a638aff0e6bf5ae8a86b26808d021f9a988
dsullivan21 <deveshsullivan21@gmail.com>
2018-12-11 21:31:49 -0600

    Update detectFace.m

--- detectFace.m
+++ detectFace.m
@@ -1,6 +1,6 @@
 function [face,eyePos] = detectFace(img)
 
-% I = imread(img);
+%I = imread('pics/7.jpg');
 I = img;
 %Return Face center x and y positions, and each side of the eyes x and y
 %positions. 
@@ -16,6 +16,13 @@ for i=1:size(bbFace,1)
     face = bbFace(i,:);
 end
 
+
+%figure, imshow(I);
+%hold on;
+%for i=1:size(bbFace,1)
+%    rectangle('Position',bbFace(i,:),'LineWidth', 5,'LineStyle','-','EdgeColor','r');
+%end
+%hold off;
 %Face Returns vector with variables of face: [x,y,width,height]
 
 %Eyes Detection
@@ -26,6 +33,7 @@ bbEyes = step(eyeDetector,I);
 [x,y,z] = size(bbEyes);
 eyes = bbEyes(x,:,:);
 
+
 %Calculate edge of eye positions
 x2 = eyes(1);
 width = eyes(4)/2;
@@ -35,12 +43,9 @@ realx = x2 - width;
 realx2 = x2 + width;
 k = imcrop(I, eyes);
 
+realx = int8(realx);
+realx2 = int8(realx2);
 % eyePos returns x values for edge of eye box, y value of center, height, and
 % width
 eyePos = [realx, realx2,eyes(2),eyes(3),eyes(4)];
-
-
-
-
-
 
